e6109e20d0d5df98bb71faa1a4697fdee50eb3e6
Manos Pavlidakis <manospavl@ics.forth.gr>
2018-01-24 14:57:15 +0200

    removed unused files

--- mesosIntegration/examples/kernelLib/nvidia_samples/darkGray/include/darkGrayArgs.h
+++
@@ -1,7 +0,0 @@
-#ifndef DARK_GREY_ARGS_HEADER
-#define DARK_GREY_ARGS_HEADER
-typedef struct {
-  int width;  /**< Image width */
-  int height; /**< image height */
-} darkGrayArgs;
-#endif
--- mesosIntegration/examples/kernelLib/nvidia_samples/histogram/include/histogramArgs.h
+++
@@ -1,6 +0,0 @@
-#ifndef HISTOGRAM_ARGS_HEADER
-#define HISTOGRAM_ARGS_HEADER
-typedef struct {
-	uint byteCount;
-} histogramArgs;
-#endif
